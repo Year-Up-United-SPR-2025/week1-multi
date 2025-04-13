c7a3414804d342e2f725f4cf8f44451acc69af6d
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-13 12:05:48 -0400

    Added comments to demo3

--- demo3/src/main/java/com/pluralsight/Main.java
+++ demo3/src/main/java/com/pluralsight/Main.java
@@ -1,9 +1,18 @@
 package com.pluralsight;
+/*
+this was written by losts of people
+Matt
+Damian
+Arthur
+
+
+ */
 
 public class Main {
 
     public static void main(String[] args){
-        //do something
-        System.out.println("hello class");
+
+        //this will display the message
+        System.out.println(/*this is the message */ "hello class" );
     }
 }
